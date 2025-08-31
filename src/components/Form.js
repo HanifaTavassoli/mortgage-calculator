fcfd3d7fec04a7f9064d3ca08c756ec25cbf3f95
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-31 09:17:36 +0430

    create basic Form component structure

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+function Form() {
+  return (
+    <form className="mortgage-form">
+      <div className="field ">
+        <span className="field-label">Mortgage Type</span>
+        <div className="radio-group"></div>
+      </div>
+
+      <div className="field"></div>
+    </form>
+  );
+}
+
+export default Form;
