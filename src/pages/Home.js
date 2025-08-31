532bfc4c45507fb87eb7a8391f68a69fb8ea0af9
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-31 10:01:31 +0430

    add all states in object and integrate Form with Home using dynamic props

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./Home.css";
 import Button from "../components/Button";
 import ResultsPanel from "../components/results/ResultsPanel";
-
+import Form from "../components/Form";
 function Home() {
   const [amount, setAmount] = useState(300000);
   const [term, setTerm] = useState(25);
@@ -11,6 +11,17 @@ function Home() {
 
   const [monthlyPay, setMonthlyPay] = useState(0);
   const [totalPay, setTotalPay] = useState(0);
+
+  const mortgageState = {
+    type,
+    setType,
+    amount: amount,
+    setAmount,
+    rate: rate,
+    setRate,
+    term: term,
+    setTerm,
+  };
 
   return (
     <>
@@ -23,6 +34,12 @@ function Home() {
 
                 <Button className="clear-btn">Clear All</Button>
               </div>
+
+              <Form
+                {...mortgageState}
+                setMonthlyPay={setMonthlyPay}
+                setTotalPay={setTotalPay}
+              />
             </div>
           </div>
           <ResultsPanel monthly={monthlyPay} total={totalPay} />
