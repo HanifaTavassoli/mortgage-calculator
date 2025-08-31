5f87183ba2a327c09ef77b074d1e013571d2d9f9
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-31 09:58:55 +0430

    add state management and import, integrate ResultsPanel comp to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import "./Home.css";
 import Button from "../components/Button";
+import ResultsPanel from "../components/results/ResultsPanel";
 
 function Home() {
+  const [amount, setAmount] = useState(300000);
+  const [term, setTerm] = useState(25);
+  const [rate, setRate] = useState(5.25);
+  const [type, setType] = useState("repayment");
+
+  const [monthlyPay, setMonthlyPay] = useState(0);
+  const [totalPay, setTotalPay] = useState(0);
+
   return (
     <>
       <div className="container">
@@ -15,6 +25,7 @@ function Home() {
               </div>
             </div>
           </div>
+          <ResultsPanel monthly={monthlyPay} total={totalPay} />
         </div>
       </div>
     </>
