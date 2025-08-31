cd3ee767bf068541cc0cf332a70af7b25ba5c447
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-31 09:19:20 +0430

    import and integrate RadioButton component into Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,26 @@
+import RadioButton from "../components/RadioButton";
+
 function Form() {
   return (
     <form className="mortgage-form">
       <div className="field ">
         <span className="field-label">Mortgage Type</span>
-        <div className="radio-group"></div>
+        <div className="radio-group">
+          <RadioButton
+            label="Repayment"
+            id="repayment"
+            value="repayment"
+            type=""
+            onChange=""
+          />
+          <RadioButton
+            label="Interest Only"
+            id="interest"
+            value="interest"
+            type=""
+            onChange="s"
+          />
+        </div>
       </div>
 
       <div className="field"></div>
