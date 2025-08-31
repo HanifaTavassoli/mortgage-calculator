0552b9384f9389574c86b60843831c90ef34750b
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-31 09:55:10 +0430

    add Home component skeleton with title

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <div className="container">
+        <div className="calculator-card">
+          <div className="calc-panel calc-panel--form">
+            <div className="panel-inner">
+              <div className="panel-header">
+                <h1 className="panel-title"> Mortgage Calculator</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
