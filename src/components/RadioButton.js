08ec216ea76c832e757587fefeebdf2ee6c2f5b5
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-31 08:59:23 +0430

    make RadioButton comp dynamic by receiving props for flexible data handling

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -1,11 +1,19 @@
-function RadioButton() {
+function RadioButton({ label, id, value, type, onChange }) {
   return (
     <>
-      <div className="radio-box">
-        <input type="radio" name="type" className="radio-input" />
-        <label className="radio-label" htmlFor="">
+      <div className="radio-box" onClick={() => onChange(value)}>
+        <input
+          type="radio"
+          name="type"
+          id={id}
+          value={value}
+          checked={type === value}
+          onChange={() => onChange(value)}
+          className="radio-input"
+        />
+        <label className="radio-label" htmlFor={id}>
           <span className="radio-faux"></span>
-          ""
+          {label}
         </label>
       </div>
     </>
