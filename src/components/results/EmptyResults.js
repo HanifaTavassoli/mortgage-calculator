89f89f26cdb34676d670fa00da0cdfd16711a374
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-31 09:44:04 +0430

    make EmptyResults component dynamic with props and import and use calcImg

--- src/components/results/EmptyResults.js
+++ src/components/results/EmptyResults.js
@@ -1,11 +1,12 @@
-function EmptyResults() {
+import calcImg from "../../assets/images/illustration-empty.svg";
+function EmptyResults({ title, desc }) {
   return (
     <>
       <div className="panel-inner results-inner results-inner-empty">
         <div className="">
-          <img src="" alt="calc img" />
-          <h2 className="results-title"></h2>
-          <p className="results-desc"></p>
+          <img src={calcImg} alt="calc img" />
+          <h2 className="results-title">{title}</h2>
+          <p className="results-desc">{desc}</p>
         </div>
       </div>
     </>
