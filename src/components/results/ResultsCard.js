60f96f13dc346e88de60bde12987cedc7f571994
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-31 09:47:20 +0430

    implement ResultsCard with ResultRow comp for monthly and total repayments

--- src/components/results/ResultsCard.js
+++ src/components/results/ResultsCard.js
@@ -1,5 +1,21 @@
+import ResultRow from "./ResultRow";
 function ResultsCard({ monthly, total }) {
-  return <div className="results-card"></div>;
+  return (
+    <div className="results-card">
+      <ResultRow
+        label="monthly"
+        text="Your monthly repayments"
+        amount={monthly}
+      />
+      <hr className="line" />
+
+      <ResultRow
+        label="total"
+        text="Total you'll repay over the term"
+        amount={total}
+      />
+    </div>
+  );
 }
 
 export default ResultsCard;
