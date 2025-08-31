a3ba4e9987922b02d4e46bcbfd8fb09ce81004ab
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-31 09:42:22 +0430

    create EmptyResults component for no-results state

---
+++ src/components/results/EmptyResults.js
@@ -0,0 +1,15 @@
+function EmptyResults() {
+  return (
+    <>
+      <div className="panel-inner results-inner results-inner-empty">
+        <div className="">
+          <img src="" alt="calc img" />
+          <h2 className="results-title"></h2>
+          <p className="results-desc"></p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default EmptyResults;
